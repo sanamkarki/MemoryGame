21dd977ef6cefd947c64f0a39f9d7778f1a72718
sanamkarki <karkisanam59@gmail.com>
2023-02-14 15:42:22 -0600

    restructuring the div

--- force-app/main/default/lwc/gameTest/gameTest.js
+++ force-app/main/default/lwc/gameTest/gameTest.js
@@ -4,6 +4,8 @@ import { loadStyle } from "lightning/platformResourceLoader";
 export default class GameTest extends LightningElement {
   disabledclick = false;
   libLoaded = false;
+  clickCount = 0;
+  remainingClicks = 3;
   clickValue1;
   clickValue2;
   iconCollection = [
@@ -80,53 +82,92 @@ export default class GameTest extends LightningElement {
       this.libLoaded = true;
     }
   }
+  // start of click event on icons
   iconClick(event) {
-    console.log("clicked Event", event.target.id.split("-")[0]);
-    if (this.disabled) {
-      return;
-    } else {
-      if (this.clickValue1) {
-        this.clickValue2 = event.target.id.split("-")[0];
-      } else {
-        this.clickValue1 = event.target.id.split("-")[0];
-      }
-      console.log("click1", this.clickValue1);
-      console.log("click2", this.clickValue2);
+    let iconElem = event.target;
+    iconElem.classList.add("open", "show", "disabled");
+    console.log("All class List", iconElem.classList);
 
-      // working with the two click values now
+    //   this.clickCount = this.clickCount + 1;
+    //   console.log("Click Count", this.clickCount);
+    //   if (this.clickCount % 2 == 0 && this.clickCount <= 6) {
+    //     this.remainingClicks = this.remainingClicks - 1;
+    //   }
 
-      if (this.clickValue1 == 1 && this.clickValue2 == 9) {
-        console.log("Winner");
-        this.disabledclick = true;
-      }
-      if (this.clickValue1 == 2 && this.clickValue2 == 10) {
-        console.log("Winner");
-        this.disabledclick = true;
-      }
-      if (this.clickValue1 == 3 && this.clickValue2 == 11) {
-        console.log("Winner");
-        this.disabledclick = true;
-      }
-      if (this.clickValue1 == 4 && this.clickValue2 == 12) {
-        console.log("Winner");
-        this.disabledclick = true;
-      }
-      if (this.clickValue1 == 5 && this.clickValue2 == 13) {
-        console.log("Winner");
-        this.disabledclick = true;
-      }
-      if (this.clickValue1 == 6 && this.clickValue2 == 14) {
-        console.log("Winner");
-        this.disabledclick = true;
-      }
-      if (this.clickValue1 == 7 && this.clickValue2 == 15) {
-        console.log("Winner");
-        this.disabledclick = true;
-      }
-      if (this.clickValue1 == 8 && this.clickValue2 == 16) {
-        console.log("Winner");
-        this.disabledclick = true;
-      }
-    }
+    //   console.log("remaining clicks:", this.remainingClicks);
+    //   console.log("clicked Event", event.target.id.split("-")[0]);
+    //   if (this.disabledclick) {
+    //     return;
+    //   } else {
+    //     if (this.clickValue1) {
+    //       this.clickValue2 = event.target.id.split("-")[0];
+    //     } else {
+    //       this.clickValue1 = event.target.id.split("-")[0];
+    //     }
+    //     console.log("click1", this.clickValue1);
+    //     console.log("click2", this.clickValue2);
+
+    //     // working with the two click values now
+
+    //     if (
+    //       (this.clickValue1 == 1 && this.clickValue2 == 9) ||
+    //       (this.clickValue1 == 9 && this.clickValue2 == 1)
+    //     ) {
+    //       console.log("Winner");
+    //       this.disabledclick = true;
+    //     } else if (
+    //       (this.clickValue1 == 2 && this.clickValue2 == 10) ||
+    //       (this.clickValue1 == 10 && this.clickValue2 == 2)
+    //     ) {
+    //       console.log("Winner");
+    //       this.disabledclick = true;
+    //     } else if (
+    //       (this.clickValue1 == 3 && this.clickValue2 == 11) ||
+    //       (this.clickValue1 == 11 && this.clickValue2 == 3)
+    //     ) {
+    //       console.log("Winner");
+    //       this.disabledclick = true;
+    //     } else if (
+    //       (this.clickValue1 == 4 && this.clickValue2 == 12) ||
+    //       (this.clickValue1 == 12 && this.clickValue2 == 4)
+    //     ) {
+    //       console.log("Winner");
+    //       this.disabledclick = true;
+    //     } else if (
+    //       (this.clickValue1 == 5 && this.clickValue2 == 13) ||
+    //       (this.clickValue1 == 13 && this.clickValue2 == 5)
+    //     ) {
+    //       console.log("Winner");
+    //       this.disabledclick = true;
+    //     } else if (
+    //       (this.clickValue1 == 6 && this.clickValue2 == 14) ||
+    //       (this.clickValue1 == 14 && this.clickValue2 == 6)
+    //     ) {
+    //       console.log("Winner");
+    //       this.disabledclick = true;
+    //     } else if (
+    //       (this.clickValue1 == 7 && this.clickValue2 == 15) ||
+    //       (this.clickValue1 == 15 && this.clickValue2 == 7)
+    //     ) {
+    //       console.log("Winner");
+    //       this.disabledclick = true;
+    //     } else if (
+    //       (this.clickValue1 == 8 && this.clickValue2 == 16) ||
+    //       (this.clickValue1 == 16 && this.clickValue2 == 8)
+    //     ) {
+    //       console.log("Winner");
+    //       this.disabledclick = true;
+    //     }
+    //   }
+    // }
+    // // working with referesh click button
+    // refreshClick() {
+    //   this.clickValue1 = null;
+    //   this.clickValue2 = null;
+    //   console.log(this.clickValue1, this.clickValue2);
+    //   this.disabledclick = false;
+    //   this.remainingClicks = 3;
+    //   this.clickCount = 0;
+    // }
   }
 }
